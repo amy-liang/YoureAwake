a94ba432f5c4994051ec5dabb03688f8f25ce721
vaishalikumanan <vaishali.kumanan@gmail.com>
2017-11-12 13:04:56 -0500

    Added more functionality to button

--- app/src/main/java/com/a2cs2tron/youreawake/MainActivity.java
+++ app/src/main/java/com/a2cs2tron/youreawake/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +14,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button button = findViewById(R.id.alarm_button);
+        final ImageView img= findViewById(R.id.imageView);
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 //Toggle Button stuff
                 if(button.getText() == "I AM AWAKE") {
                     //Music stuff?
                     //Move/Switch image
+                    img.setImageResource(R.mipmap.alarm2);
                     button.setText("OK");
                 } else {
                     //Stop music?
+                    //Move/Switch image
+                    img.setImageResource(R.mipmap.alarm1);
                     button.setText("I AM AWAKE");
                 }
             }
