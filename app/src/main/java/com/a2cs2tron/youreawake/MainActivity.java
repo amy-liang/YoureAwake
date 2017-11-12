86e3c54f57044d01761815bfbc4fd7ad252340a7
vaishalikumanan <vaishali.kumanan@gmail.com>
2017-11-12 13:54:09 -0500

    Notifications

--- app/src/main/java/com/a2cs2tron/youreawake/MainActivity.java
+++ app/src/main/java/com/a2cs2tron/youreawake/MainActivity.java
@@ -1,5 +1,10 @@
 package com.a2cs2tron.youreawake;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +23,25 @@ public class MainActivity extends AppCompatActivity {
         final ImageView img= findViewById(R.id.imageView);
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.century);
 
+        String CHANNEL_ID = "my_channel_01";
+        final NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.mipmap.alarm1)
+                        .setContentTitle("YOU'RE AWAKE")
+                        .setContentText("YOU'RE AWAKE!")
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setPriority(NotificationManager.IMPORTANCE_HIGH);
+        Intent resultIntent = new Intent(this, ResultActivity.class);
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setAutoCancel(true);
+        mBuilder.setContentIntent(resultPendingIntent);
+
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 //Toggle Button stuff
@@ -35,6 +59,12 @@ public class MainActivity extends AppCompatActivity {
                     //Move/Switch image
                     img.setImageResource(R.mipmap.alarm2);
                     button.setText("OK");
+                    int mNotificationId = 001;
+                    // Gets an instance of the NotificationManager service
+                    NotificationManager mNotifyMgr =
+                            (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    // Builds the notification and issues it.
+                    mNotifyMgr.notify(mNotificationId, mBuilder.build());
                 }
             }
         });
