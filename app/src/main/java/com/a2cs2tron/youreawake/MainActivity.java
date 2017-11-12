0b8e30af78d320ba1ff130d1a672830153e02f77
vaishalikumanan <vaishali.kumanan@gmail.com>
2017-11-12 12:52:07 -0500

    Added onclick button event

--- app/src/main/java/com/a2cs2tron/youreawake/MainActivity.java
+++ app/src/main/java/com/a2cs2tron/youreawake/MainActivity.java
@@ -2,6 +2,8 @@ package com.a2cs2tron.youreawake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.alarm_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //Toggle Button stuff
+                if(button.getText() == "I AM AWAKE") {
+                    //Music stuff?
+                    //Move/Switch image
+                    button.setText("OK");
+                } else {
+                    //Stop music?
+                    button.setText("I AM AWAKE");
+                }
+            }
+        });
     }
+
 }
